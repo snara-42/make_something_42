71c7905752fd5a8127e9882a37494e4974357067
seonseo <seonseo@student.42seoul.kr>
2024-05-27 21:07:25 +0900

    - show a list of possible subjects a user can choose from by number - only show quiz questions that the user hasn't gotten right yet

--- seonseo/memorizer.py
+++ seonseo/memorizer.py
@@ -92,44 +92,85 @@ projects = {
     },
 }
 
-# Flatten the projects dictionary into function_headers
-function_headers = {func: header for project in projects.values() for func, header in project.items()}
+def add_function(function, header, subject):
+    if subject in projects:
+        projects[subject][function] = header
+        print(f"Added: {function} -> {header} to subject {subject}")
+    else:
+        projects[subject] = {function: header}
+        print(f"Added new subject {subject} with: {function} -> {header}")
 
-def add_function(function, header):
-    function_headers[function] = header
-    print(f"Added: {function} -> {header}")
+def view_functions(subject=None):
+    if subject:
+        if subject in projects:
+            for function, header in projects[subject].items():
+                print(f"{function} -> {header}")
+        else:
+            print(f"Subject {subject} not found.")
+    else:
+        for subject, funcs in projects.items():
+            print(f"\nSubject: {subject}")
+            for function, header in funcs.items():
+                print(f"{function} -> {header}")
 
-def view_functions():
-    for function, header in function_headers.items():
-        print(f"{function} -> {header}")
+def list_subjects():
+    subjects = list(projects.keys())
+    for i, subject in enumerate(subjects):
+        print(f"{i+1}. {subject}")
+    return subjects
 
-def quiz():
-    functions = list(function_headers.keys())
-    while True:
-        random_function = random.choice(functions)
+def quiz(subject):
+    if subject not in projects:
+        print(f"Subject {subject} not found.")
+        return
+
+    functions = list(projects[subject].keys())
+    incorrect_answers = set(functions)  # Track functions that were answered incorrectly or not answered yet
+
+    while incorrect_answers:
+        random_function = random.choice(list(incorrect_answers))
         user_input = input(f"Which header does '{random_function}' belong to? (enter 'quit' to stop) ")
         if user_input.lower() == 'quit':
             print("Quiz ended.")
             break
-        elif user_input == function_headers[random_function]:
+        elif user_input == projects[subject][random_function]:
             print("Correct!")
+            incorrect_answers.remove(random_function)  # Remove correct answer from the set
         else:
-            print(f"Incorrect. The correct answer is '{function_headers[random_function]}'.")
+            print(f"Incorrect. The correct answer is '{projects[subject][random_function]}'.")
+
+    if not incorrect_answers:
+        print("You have answered all questions correctly for this subject!")
 
 def main():
     parser = argparse.ArgumentParser(description='CLI for memorizing library functions and headers.')
-    parser.add_argument('--add', nargs=2, metavar=('FUNCTION', 'HEADER'), help='Add a function and its header')
-    parser.add_argument('--view', action='store_true', help='View all functions and headers')
-    parser.add_argument('--quiz', action='store_true', help='Quiz yourself on functions and headers')
+    parser.add_argument('--add', nargs=3, metavar=('FUNCTION', 'HEADER', 'SUBJECT'), help='Add a function and its header to a subject')
+    parser.add_argument('--view', nargs='?', const=True, metavar='SUBJECT', help='View all functions and headers, optionally for a specific subject')
+    parser.add_argument('--quiz', nargs='?', const=True, metavar='SUBJECT', help='Quiz yourself on functions and headers for a specific subject')
 
     args = parser.parse_args()
 
     if args.add:
-        add_function(args.add[0], args.add[1])
+        add_function(args.add[0], args.add[1], args.add[2])
     elif args.view:
-        view_functions()
+        if args.view is True:
+            view_functions()
+        else:
+            view_functions(args.view)
     elif args.quiz:
-        quiz()
+        if args.quiz is True:
+            print("Please choose a subject from the list below by number:")
+            subjects = list_subjects()
+            try:
+                choice = int(input("Enter the number of the subject: ")) - 1
+                if 0 <= choice < len(subjects):
+                    quiz(subjects[choice])
+                else:
+                    print("Invalid choice.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+        else:
+            quiz(args.quiz)
     else:
         parser.print_help()
 
