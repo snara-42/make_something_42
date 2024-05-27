40b5124c892190e8077c7153398c54d07a299c90
subaru nara <snara@student.42tokyo.jp>
2024-05-27 18:53:49 +0900

    add more messages

--- fortune.py
+++ fortune.py
@@ -1,14 +1,20 @@
+#!python3
+
 import random
 
 
 def fortune():
     unlucky = [
-        "segmentation fault", "norme error", "bus error", "double free",
-        "uninitialized variables", "overflow", "memory leak"
+        "segmentation fault", "norme error", "bus error", "double free", "infinite loop",
+        "uninitialized variables", "overflow", "memory leak", "overwork",
+        "TIG", "stumbling on a cable", "running out of memory",
     ]
-    lucky = ["everything", "your teamwork", "exam", "lunch", "debugging"]
+    lucky = [
+        "everything", "your teamwork", "exam", "lunch", "debugging", "review", "nap",
+    ]
     msg = f"today beware of {random.choice(unlucky)}, but {random.choice(lucky)} shall go well!"
     print(msg)
+
 
 if __name__ == "__main__":
     fortune()
