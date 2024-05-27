f97b81624c37a57408ddc285227392e48521bb06
jihkang <106561135+jihkang@users.noreply.github.com>
2024-05-27 22:13:36 +0900

    feat: discord bot setup for using quiz and store data

---
+++ bot/response.py
@@ -0,0 +1,15 @@
+
+"""
+    not yet save high score quiz
+    show quiz result 
+"""
+
+def get_response(message: str) -> str:
+    if "get score" in message:
+        return "get high score in database"
+    else:
+        """
+        this will be added more 
+        """
+        return "none" 
+    
